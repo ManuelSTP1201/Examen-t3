d3a6dc58c066f64d83753dae9927cba02fa106b9
ManuelSTP1201 <juanmasantacruz12@gmail.com>
2024-11-14 08:42:39 -0500

    Primer commit

---
+++ Examen T3/CitaUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_T3
+{
+    public static class CitaUtil
+    {
+        public static Cita CrearCita(int numero, int codigo, string nombre, string universidad, string enfermedad, double precio)
+        {
+            var estudiante = new Estudiante(codigo, nombre, universidad);
+            return new Cita(numero, estudiante, enfermedad, precio);
+        }
+        public static void ListarCitas(Cita[] citas)
+        {
+            double sumaPrecios = 0;
+            foreach (var cita in citas)
+            {
+                if (cita != null)
+                {
+                    Console.WriteLine(cita);
+                    sumaPrecios += cita.Precio;
+                }
+            }
+            Console.WriteLine($"\nSuma total de precios: {sumaPrecios:C}");
+        }
+
+        public static void ModificarUniversidades(Cita[] citas, string textoBuscado, string textoReemplazo)
+        {
+            foreach (var cita in citas)
+            {
+                if (cita != null && cita.Estudiante.Universidad.Contains(textoBuscado))
+                {
+                    cita.Estudiante.Universidad = cita.Estudiante.Universidad.Replace(textoBuscado, textoReemplazo);
+                }
+            }
+        }
+    }
+}
---
+++ Examen T3/Program.cs
@@ -0,0 +1,24 @@
+﻿string opcion;
+
+void MostrarOpciones()
+{
+    Console.WriteLine("a)*Crear*\n");
+    Console.WriteLine("b)-Listar-\n");
+    Console.WriteLine("c)-Modificar Masivo-\n");
+    Console.WriteLine("d)-Fin-\n");
+    Console.WriteLine("Seleccione una opción: ");
+}
+
+bool ContinuarProgam=true;
+while (ContinuarProgam)
+{
+    MostrarOpciones();
+    opcion=Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "a":
+            Console.WriteLine("\nIngrese su número: ");
+            break;
+    }
+}
